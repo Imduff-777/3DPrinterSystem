ca25835ac141be4ce0226e62fd83c4ab20ae7251
erick <erickbrad9@gmail.com>
2025-11-24 20:40:27 -0300

    Public routes criado

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from 'express'
+import publicRoutes from './routes/public.js'
 
 const app = express()
+app.use(express.json())
+
+app.use('/', publicRoutes)
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`Server rodando na porta ${port}`)
-})
-
-app.get("/", (req, res) => {
-    res.send({message: "hello world"})
 })
